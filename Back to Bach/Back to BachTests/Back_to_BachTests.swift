6fbfab6b104b3d1d349b9531d4575f70d1eb64c4
Flavius Condurache <Orion@Flaviuss-Mac-Pro.local>
2015-06-23 14:47:05 +0300

    Added Back to Bach App\!

---
+++ Back to Bach/Back to BachTests/Back_to_BachTests.swift
@@ -0,0 +1,36 @@
+//
+//  Back_to_BachTests.swift
+//  Back to BachTests
+//
+//  Created by Flavius Condurache on 22/06/15.
+//  Copyright (c) 2015 Flavius Condurache. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Back_to_BachTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
